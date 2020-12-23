afe145d38befad2d928aec85a776178c53230bb1
Joseph Cappadona <josephcappadona27@gmail.com>
2020-12-23 05:57:35 -0500

    separate functionality into dev dataset object

--- kids_britannica/dataset.py
+++ kids_britannica/dataset.py
@@ -1,37 +1,15 @@
 from .imports import *
 from .utils import *
 from .urls import *
-from .download import ArticleDownloader, sanitize_filename
-from .scraping import get_article_text, parse_article_from_html
+from .download import ArticleDownloader
 
-# class Article(File):
-#     def __init__(self, filename):
-#         super().__init__(self, open(filename))
-#         #self['adjacent_ids'] = {get_tier_from_url(url):get_id_from_url(url) for url in get_adjacent_articles(self['htmls']['text'])}
-
-#Article = edict.LazyLoad
-class Article(edict.LazyLoad):
-    def __init__(self, path):
-        path = Path(path)
-        if path.exists():
-            super().__init__(path)
-        else:
-            try:
-                #print(f"Could not find article ({str(path)}) at expected path.")
-                # find the path that matches the ID
-                query = '/'.join(path.parts[:-1]) + '/' + path.parts[-1].split(' ')[0] + ' *.json'
-                match = glob(query)[0]
-                path = Path(match)
-                super().__init__(path)
-            except:
-                raise ValueError(f"Could not find article {str(path)}")
+Article = edict.LazyLoad
 
 class KidsBritannicaDataSet:
-    def __init__(self, data_dir='data', username=None, password=None):
+    def __init__(self, data_dir='data'):
         make_directories(data_dir)
         self.data_dir = Path(data_dir)
         self.articles_dir = self.data_dir / 'articles'
-        self.session = login(username, password)
         self.kids_article_paths = KidsBritannicaDataSet.get_article_paths(self.data_dir, tier='kids')
         self.students_article_paths = KidsBritannicaDataSet.get_article_paths(self.data_dir, tier='students')
         self.scholars_article_paths = KidsBritannicaDataSet.get_article_paths(self.data_dir, tier='scholars')
@@ -105,40 +83,6 @@ class KidsBritannicaDataSet:
                 print('Writing metadata to file...')
                 write_json(self.metadata_filepath, self._metadata)
         return self._metadata
-
-    def download_urls(self, overwrite=True, limit=None, verbose=1):
-        data_dir = self.data_dir
-        urls_filepath = Path(data_dir) / 'urls.json'
-        if not urls_filepath.exists() or overwrite:
-            all_urls = get_all_urls(session=self.session, limit=limit, verbose=verbose)
-
-            write_json(urls_filepath, all_urls)
-            return all_urls
-    
-    def download_articles(self, urls=None, limit=None, **kwargs):
-        if urls is None:
-            url_json_path = self.data_dir / 'urls.json'
-            if not url_json_path.exists():
-                print("No URLs downloaded yet. Downloading now...")
-                urls = KidsBritannicaDataSet.download_urls(data_dir=self.data_dir, limit=limit)
-            else:
-                print("Loading URLs from file...")
-                urls = json.load(open(url_json_path, 'rt'))
-
-        print("Loading saved article IDs...")
-        saved_ids = get_saved_ids(self.data_dir)
-        urls = [url for url in urls if get_id_from_url(url) not in saved_ids] # filter
-        if limit:
-            urls = rand.choices(urls, k=limit) # random selection
-        print(f"{len(saved_ids)} articles already downloaded.")
-        print(f"Downloading {len(urls)} articles...")
-
-        dl = ArticleDownloader(data_dir=self.data_dir, session=self.session, **kwargs)
-        dl.download_urls(urls)
-    
-    @staticmethod
-    def download_media(data_dir='data'):
-        raise NotImplemented
     
     @property
     def statistics(self):
@@ -182,15 +126,58 @@ class KidsBritannicaDataSet:
             self._structures = structures
         return self._structures
 
+class KidsBritannicaDataSetDev(KidsBritannicaDataSet):
+    
+    def __init__(self, *args, **kwargs, username=None, password=None):
+        super().__init__(*args, **kwargs)
+        if username and password:
+            self.session = login(username, password)
+
     @staticmethod
     def get_article_paths(data_dir, tier='*'):
         article_paths = glob(str(data_dir / 'articles' / tier / '*.json'))
         if len(article_paths) == 0:
             raise ValueError(f"No articles could not be found in {str(data_dir)}. Please download the data first.")
         return article_paths
+
+    def download_urls(self, overwrite=True, limit=None, verbose=1):
+        data_dir = self.data_dir
+        urls_filepath = Path(data_dir) / 'urls.json'
+        if not urls_filepath.exists() or overwrite:
+            all_urls = get_all_urls(session=self.session, limit=limit, verbose=verbose)
+
+            write_json(urls_filepath, all_urls)
+            return all_urls
+    
+    def download_articles(self, urls=None, limit=None, **kwargs):
+        from .download import ArticleDownloader
+        if urls is None:
+            url_json_path = self.data_dir / 'urls.json'
+            if not url_json_path.exists():
+                print("No URLs downloaded yet. Downloading now...")
+                urls = KidsBritannicaDataSet.download_urls(data_dir=self.data_dir, limit=limit)
+            else:
+                print("Loading URLs from file...")
+                urls = json.load(open(url_json_path, 'rt'))
+
+        print("Loading saved article IDs...")
+        saved_ids = get_saved_ids(self.data_dir)
+        urls = [url for url in urls if get_id_from_url(url) not in saved_ids] # filter
+        if limit:
+            urls = rand.choices(urls, k=limit) # random selection
+        print(f"{len(saved_ids)} articles already downloaded.")
+        print(f"Downloading {len(urls)} articles...")
+
+        dl = ArticleDownloader(data_dir=self.data_dir, session=self.session, **kwargs)
+        dl.download_urls(urls)
+    
+    @staticmethod
+    def download_media(data_dir='data'):
+        raise NotImplemented
     
     @staticmethod
     def write_articles_from_html(data_dir, overwrite=False):
+        from .scraping import parse_article_from_html
         data_dir = Path(data_dir)
         make_directories(data_dir)
         html_paths = glob(str(data_dir / 'html' / '*.json'))
